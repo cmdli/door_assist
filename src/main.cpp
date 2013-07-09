52fc23a6dd5014214a33c7f21382a875193ac814
Chris de la Iglesia <cmdli@utexas.edu>
2013-07-09 15:09:45 -0500

    Changed localization to use existing TF

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,40 @@
 
 #include <ros/ros.h>
-#include "tf.h"
-#include <geometry_msgs.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PoseStamped.h>
 
 #define PI 3.14
-
-geometry_msgs::Pose location;
-
-void callback(geometry_msgs::PoseWithCovarianceStamped pose)
-{
-  location = pose.pose.pose; // -.-
-}
 
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "door_assist");
-  if(argc < 7) {
-    ROS_ERROR("Correct door_assist usage: rosrun door_assist position_x position_y position_z orientation_x orientation_y orientation_z orientation_w");
-  }
 
   ros::NodeHandle n;
-  ros::Publisher move_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal");
-  ros::Subscriber get_location = n.subscribe<geometry_msgs::PoseWithCovarianceStamped>("amcl_pose", 1, callback);
+  ros::Publisher move_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1);
 
-  location.position.x = -1.0;
-
-  while(ros::ok() && location.position.x == -1.0) {
-    ros::spinOnce();
+  tf::TransformListener listener;
+  tf::StampedTransform transform;
+  try {
+    listener.lookupTransform("/base_footprint", "/map",
+			     ros::Time(0), transform);
+  }
+  catch(tf::TransformException ex) {
+    ROS_ERROR("%s", ex.what());
+    return 1;
   }
 
   geometry_msgs::Point pos;
-  geometry_msgs::Quaternion orient;
-  pos = location.position;
-  orient = location.orient;
-
-  /*  pos.x = atof(argv[0]);
-  pos.y = atof(argv[1]);
-  pos.z = atof(argv[2]);
-  orient.x = atof(argv[3]);
-  orient.y = atof(argv[4]);
-  orient.z = atof(argv[5]);
-  orient.w = atof(argv[6]);*/
+  tf::Quaternion orient;
+  pos.x = transform.getOrigin().x();
+  pos.y = transform.getOrigin().y();
+  pos.z = transform.getOrigin().z();
+  orient = transform.getRotation();
 
   double angle = tf::getYaw(orient);
   double xd = 0.5*cos(angle);
   double zd = 0.5*sin(angle);
   
-  Quaternion q;
+  tf::Quaternion q;
   q.setEuler(PI/2,0,0);
   orient += q;
 
@@ -54,9 +42,14 @@ int main(int argc, char* argv[])
   goal.pose.position.x = pos.x + xd;
   goal.pose.position.y = pos.y;
   goal.pose.position.z = pos.x + zd;
-  goal.pose.orientation = orient;
+  goal.pose.orientation.x = orient.getX();
+  goal.pose.orientation.y = orient.getY();
+  goal.pose.orientation.z = orient.getZ();
+  goal.pose.orientation.w = orient.getW();
 
   move_pub.publish(goal);
+
+  ROS_INFO("Goal Published");
 
   ros::spinOnce();
     
